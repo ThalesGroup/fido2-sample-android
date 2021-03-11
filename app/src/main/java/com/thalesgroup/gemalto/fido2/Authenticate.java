5b8effeb18a4cb9db7bce77d2d82b1bb5dfc6f14
ManisDroid <62420577+ManisDroid@users.noreply.github.com>
2021-03-12 06:18:48 +0800

    Remove the import statements

--- app/src/main/java/com/thalesgroup/gemalto/fido2/Authenticate.java
+++ app/src/main/java/com/thalesgroup/gemalto/fido2/Authenticate.java
@@ -3,23 +3,10 @@ package com.thalesgroup.gemalto.fido2;
 
 import androidx.fragment.app.FragmentActivity;
 
-import com.thalesgroup.gemalto.fido2.authenticator.biometric.BiometricAuthenticatorCallback;
-import com.thalesgroup.gemalto.fido2.authenticator.passcode.PasscodeAuthenticator;
-import com.thalesgroup.gemalto.fido2.authenticator.passcode.PasscodeAuthenticatorCallback;
-import com.thalesgroup.gemalto.fido2.client.Fido2Client;
-import com.thalesgroup.gemalto.fido2.client.Fido2ClientFactory;
-import com.thalesgroup.gemalto.fido2.client.Fido2Request;
-import com.thalesgroup.gemalto.fido2.client.Fido2RespondArgs;
-import com.thalesgroup.gemalto.fido2.client.Fido2Response;
-import com.thalesgroup.gemalto.fido2.client.Fido2ResponseCallback;
-import com.thalesgroup.gemalto.fido2.client.Fido2UiCallback;
 import com.thalesgroup.gemalto.fido2.sample.Configuration;
 import com.thalesgroup.gemalto.fido2.sample.domain.logger.Logger;
 import com.thalesgroup.gemalto.fido2.sample.ui.fragment.HomeFragment;
 import com.thalesgroup.gemalto.fido2.sample.util.JsonUtil;
-import com.thalesgroup.gemalto.fido2.ui.SampleBiometricAuthenticatorCallback;
-import com.thalesgroup.gemalto.fido2.ui.SampleFido2UiCallback;
-import com.thalesgroup.gemalto.fido2.ui.SamplePasscodeAuthenticatorCallback;
 
 public class Authenticate {
     private FragmentActivity activity;
@@ -41,7 +28,7 @@ public class Authenticate {
                         "          \"rpId\" : \""+ Configuration.rpId+"\"" +
                         "        }";
 
-                // Log Registration request json string into Log view.
+                // Log Authentication request json string into Log view.
                 logger.log("Authentication Request:\n" + JsonUtil.prettyPrintJSON(jsonString));
 
                 # 1: Create Authentication request providing the required credentials. #
